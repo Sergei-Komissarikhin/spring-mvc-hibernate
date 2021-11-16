4f6dc787c442b62d90d28ff1d0707d40b6a31036
Komissarikhin-S <komissarihin.s@yandex.ru>
2021-11-16 15:50:29 +0300

    From Work to home

---
+++ src/main/java/web/controllers/UserController.java
@@ -0,0 +1,35 @@
+package web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import web.service.UserService;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public String index(Model model){
+        model.addAttribute("users",userService.getAllUsers());
+        return "users/index";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable("id") long id, Model model){
+        model.addAttribute("user", userService.getUserById(id));
+        return "users/show";
+    }
+
+
+}
